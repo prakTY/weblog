2d52504cb58173131cf3bd5c308db96270627e7d
朱天宇 <1019427682@qq.com>
2026-03-17 18:09:12 +0800

    搭建 Spring Boot 多模块工程（通过 Spring Initializr）

---
+++ weblog-module-common/src/test/java/com/prakty/weblog/common/WeblogModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakty.weblog.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
