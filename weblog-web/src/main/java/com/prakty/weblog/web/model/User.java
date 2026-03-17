95ffaa3f680fd95c8f322607be63a1e267cf92dc
朱天宇 <1019427682@qq.com>
2026-03-17 20:21:22 +0800

    Spring Boot 自定义注解，实现 API 请求日志切面

---
+++ weblog-web/src/main/java/com/prakty/weblog/web/model/User.java
@@ -0,0 +1,18 @@
+package com.prakty.weblog.web.model;
+
+import lombok.Data;
+
+/**
+ * @author: 犬小哈
+ * @url: www.quanxiaoha.com
+ * @date: 2023-08-10 10:35
+ * @description: TODO
+ **/
+@Data
+public class User {
+    // 用户名
+    private String username;
+    // 性别
+    private Integer sex;
+}
+
