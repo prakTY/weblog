97a38df8be0edb029ddcc54eb4c95afce40a00c3
朱天宇 <1019427682@qq.com>
2026-03-18 22:15:36 +0800

    Spring Boot 实现优雅的参数校验

--- weblog-web/src/main/java/com/prakty/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/prakty/weblog/web/controller/TestController.java
@@ -3,9 +3,15 @@ package com.prakty.weblog.web.controller;
 import com.prakty.weblog.web.model.User;
 import com.prakty.weblog.common.aspect.ApiOperationLog;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 /**
  * @author: 犬小哈
@@ -17,11 +23,30 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
+    /*
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
     public User test(@RequestBody User user) {
         // 返参
         return user;
+    }
+    */
+    @PostMapping("/test")
+    @ApiOperationLog(description = "测试接口")
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
+        // 返参
+        return ResponseEntity.ok("参数没有任何问题");
     }
 
 }
