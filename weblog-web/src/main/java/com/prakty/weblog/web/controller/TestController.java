8ba822e31353057f8cbe9162507cb5ed0a740a35
朱天宇 <1019427682@qq.com>
2026-03-18 22:26:57 +0800

    Spring Boot 自定义响应工具类

--- weblog-web/src/main/java/com/prakty/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/prakty/weblog/web/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.prakty.weblog.web.controller;
 
+import com.prakty.weblog.common.utils.Response;
 import com.prakty.weblog.web.model.User;
 import com.prakty.weblog.common.aspect.ApiOperationLog;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +32,7 @@ public class TestController {
         return user;
     }
     */
+    /*
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
     public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
@@ -48,5 +50,25 @@ public class TestController {
         // 返参
         return ResponseEntity.ok("参数没有任何问题");
     }
+
+     */
+    @PostMapping("/test")
+    @ApiOperationLog(description = "测试接口")
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return Response.fail(errorMsg);
+        }
+
+        // 返参
+        return Response.success();
+    }
+
 
 }
