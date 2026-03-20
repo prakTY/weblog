f07d956559c7163f5cdc7ea14a7ebb09b631d559
朱天宇 <1019427682@qq.com>
2026-03-20 22:06:55 +0800

    整合 Mybatis Plus

--- weblog-web/src/test/java/com/prakty/weblog/web/WeblogWebApplicationTests.java
+++ weblog-web/src/test/java/com/prakty/weblog/web/WeblogWebApplicationTests.java
@@ -1,15 +1,37 @@
 package com.prakty.weblog.web;
 
+import com.prakty.weblog.common.domain.dos.UserDO;
+import com.prakty.weblog.common.domain.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 @Slf4j
 class WeblogWebApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void insertTest() {
+        // 构建数据库实体类
+        UserDO userDO = UserDO.builder()
+                .username("犬小哈")
+                .password("123456")
+                .createTime(new Date())
+                .updateTime(new Date())
+                .isDeleted(false)
+                .build();
+
+        userMapper.insert(userDO);
     }
 
     @Test
